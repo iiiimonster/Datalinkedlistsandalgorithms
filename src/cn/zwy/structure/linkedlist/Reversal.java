8b9ba0c729d24c22deefc7db2331e68bae5fe491
zwy <xiaoyang@heypad.cn>
2022-06-15 18:13:34 +0800

    数据结构

---
+++ src/cn/zwy/structure/linkedlist/Reversal.java
@@ -0,0 +1,22 @@
+package cn.zwy.structure.linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Description: 单链表反转
+ * @author: zwy
+ * @date: 2022年06月15日 17:53
+ */
+public class Reversal {
+
+    public static void reversal(Object[] objects) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
