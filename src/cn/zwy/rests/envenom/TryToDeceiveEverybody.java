d33e3e591f382a182d38081a191c7c98ec133190
zwy <xiaoyang@heypad.cn>
2022-06-21 18:00:07 +0800

    添加链接

--- src/cn/zwy/rests/envenom/TryToDeceiveEverybody.java
+++ src/cn/zwy/rests/envenom/TryToDeceiveEverybody.java
@@ -1,13 +1,14 @@
 package cn.zwy.rests.envenom;
 
 /**
- * &#064;Description:  瞒天过海 通过unicode使他人看不懂代码
+ * &#064;Description:  瞒天过海 通过unicode使他人看不懂代码   // \u000d 等于下一行
  * &#064;author:  zwy
  * &#064;date:  2022年06月16日 16:39
  */
 public class TryToDeceiveEverybody {
 
     public static void main(String[] args) {
-        // \u000d \u0053\u0079\u0073\u0074\u0065\u006d\u002e\u006f\u0075\u0074\u002e\u0070\u0072\u0069\u006e\u0074\u006c\u006e\u0028\u0022\u54c8\u54c8\u55e8\u0022\u0029;
+
+        System.out.println();
     }
 }
--- src/cn/zwy/structure/linkedlist/entity/SingleLinkedList.java
+++ src/cn/zwy/structure/linkedlist/entity/SingleLinkedList.java
@@ -2,17 +2,46 @@ package cn.zwy.structure.linkedlist.entity;
 
 /**
  * @Description: 简易链表
+ *
+ * https://blog.csdn.net/weixin_49329785/article/details/121699280?ops_request_misc=&request_id=&biz_id=102&utm_term=%E5%8D%95%E9%93%BE%E8%A1%A8&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-2-121699280.142
  * @author: zwy
  * @date: 2022年06月17日 15:16
  */
 public class SingleLinkedList {
 
-    private int size;
-    private node head;
+
+    private int Length;
+    private Node head;
 
 
-    private class node {
-        private int data;
-        private node next;
+    public SingleLinkedList(int head) {
+        Length = length;
     }
+
+    public int length(){
+        return this.Length();
+    }
+
+    //判断所需要操作的位置节点是否存在
+    public void outLength(int index){
+        if (size)
+    }
+
+    private class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "节点{" +
+                    "数据=" + data + '}';
+        }
+
+    }
+
+
 }
