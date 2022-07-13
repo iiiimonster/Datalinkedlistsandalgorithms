eb8d3f6c99cd09db71ef42cceb4354019cc82584
zwy <xiaoyang@heypad.cn>
2022-07-13 17:55:50 +0800

    删除排序链表中的重复元素 https://leetcode.cn/problems/remove-duplicates-from-sorted-list/

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # 学习数据结构与算法
 ```
-链表,数组类型  题目练习 
-力扣：
-203. 移除链表元素 https://leetcode.cn/problems/remove-linked-list-elements/
-83. 删除排序链表中的重复元素 https://leetcode.cn/problems/remove-duplicates-from-sorted-list/
-剑指offer22: 链表中倒数第k个节点 https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
-剑指offer06:  从尾到头打印链表 https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+链表类型  题目练习 
+实体类：
+SingleLinkedList  简易无头单链表 
+SinglyLinkedListOfLeadingNodes  泛型带头节点单链表
+WaterMarginLinkedList  水浒传链表练习 带头链表
 
-使用两种方式实现，
+力扣：
+SingleLinkedList 32行递归版本 59行普通版本  203. 移除链表元素 https://leetcode.cn/problems/remove-linked-list-elements/
+83. 删除排序链表中的重复元素 https://leetcode.cn/problems/remove-duplicates-from-sorted-list/
+Offer06 剑指offer22: 链表中倒数第k个节点 https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+Offer22 剑指offer06:  从尾到头打印链表 https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+
+使用两种方式实现：
 （1）根据题目使用暴力破解
 （2）上面实现后在网上寻找最优解
 
@@ -16,11 +21,8 @@
 3.MergeTwoSortedLists  两个有序的链表合并 https://leetcode.cn/problems/merge-two-sorted-lists/
 4.MiddleOfTheLinkedList 求链表的中间结点  https://leetcode.cn/problems/middle-of-the-linked-list/
 ```
-
 ```
-算法入门  题目练习 
-力扣：
-1.BinarySearch 二分查找 https://leetcode.cn/problems/binary-search/
+链表,数组类型  题目练习 
 
 ```
 
---
+++ src/cn/zwy/structure/linkedlist/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,77 @@
+package cn.zwy.structure.linkedlist.leetcode;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * &#064;Description:  <a href="https://leetcode.cn/problems/remove-duplicates-from-sorted-list/">83. 删除排序链表中的重复元素</a>
+ * &#064;author:  zwy
+ * &#064;date:  2022年07月13日 11:14
+ */
+public class RemoveDuplicatesFromSortedList {
+    /**
+     * 思路二：一次遍历（链表为升序链表）
+     *
+     * @param head 处理链表
+     * @return 返回链表结果
+     */
+    public ListNode deleteDuplicatesTwo(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode temp = head;
+        while (temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 思路一： 利用set的唯一特性 迭代版本 (链表无需排序)
+     *
+     * @param head 处理链表
+     * @return 返回链表结果
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = head.next;
+        ListNode tempt = head;
+        Set<Integer> integers = new HashSet<>();
+        integers.add(head.val);
+        while (temp != null) {
+            boolean add = integers.add(temp.val);
+            if (add) {
+                tempt = tempt.next;
+            } else {
+                tempt.next = tempt.next.next;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
