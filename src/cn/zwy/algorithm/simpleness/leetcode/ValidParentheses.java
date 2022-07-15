d004c5bd33aa333830181e16ccf870b8d893d2eb
zwy <xiaoyang@heypad.cn>
2022-07-15 17:59:53 +0800

    https://leetcode.cn/problems/valid-parentheses/">有效的括号

--- src/cn/zwy/algorithm/simpleness/leetcode/ValidParentheses.java
+++ src/cn/zwy/algorithm/simpleness/leetcode/ValidParentheses.java
@@ -1,5 +1,7 @@
 package cn.zwy.algorithm.simpleness.leetcode;
 
+import java.util.*;
+
 /**
  * &#064;Description:    <a href="https://leetcode.cn/problems/valid-parentheses/">有效的括号</a> </BR>
  * 有效字符串需满足：
@@ -23,10 +25,27 @@ public class ValidParentheses {
      * @return 返回是否为有效括号
      */
     public boolean isValid(String s) {
-        if (s.length()%2 ==1){
+        int length = s.length();
+        if (length % 2 == 1) {
             return false;
         }
-        byte[] bytes = s.getBytes();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
 
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            Character c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || stack.peek() != map.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
     }
 }
