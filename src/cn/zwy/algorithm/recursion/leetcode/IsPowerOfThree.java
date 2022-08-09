90113cc06203e6d4dce0c67335e673d102bde7a4
zwy <xiaoyang@heypad.cn>
2022-08-09 19:13:41 +0800

    预测赢家  FibonacciNumber https://leetcode.cn/problems/predict-the-winner/

--- src/cn/zwy/algorithm/recursion/leetcode/IsPowerOfThree.java
+++ src/cn/zwy/algorithm/recursion/leetcode/IsPowerOfThree.java
@@ -1,18 +1,47 @@
 package cn.zwy.algorithm.recursion.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * &#064;Description:  <a href="https://leetcode.cn/problems/power-of-three/">3 的幂</a>
  * &#064;author:  zwy
  * &#064;date:  2022年08月06日 17:23
  */
 public class IsPowerOfThree {
-    public boolean isPowerOfThree(int n) {
-        while (true){
-           int a = 3>>>3;
+    public boolean isPowerOfThreeTwo(int n) {
+        return n != 0 && n % 3 == 0 ? isPowerOfThree(n / 3) : n == 1;
+    }
+    Set<Integer> set = new HashSet<Integer>() {
+        {
+            add(1);
+            add(3);
+            add(9);
+            add(27);
+            add(81);
+            add(243);
+            add(729);
+            add(2187);
+            add(6561);
+            add(19683);
+            add(59049);
+            add(177147);
+            add(531441);
+            add(1594323);
+            add(4782969);
+            add(14348907);
+            add(43046721);
+            add(129140163);
+            add(387420489);
+            add(1162261467);
         }
+    };
+    public boolean isPowerOfThree(int n) {
+        return set.contains(n);
     }
 
     public static void main(String[] args) {
-        System.out.println(3>>>1);
+        IsPowerOfThree isPowerOfThree = new IsPowerOfThree();
+        System.out.println(isPowerOfThree.isPowerOfThree(27));
     }
 }
---
+++ src/cn/zwy/algorithm/recursion/leetcode/PredictTheWinner.java
@@ -0,0 +1,18 @@
+package cn.zwy.algorithm.recursion.leetcode;
+
+/**
+ * &#064;Description:  <a href="https://leetcode.cn/problems/predict-the-winner/">预测赢家</a> <BR/>
+ * 给你一个整数数组 nums 。玩家 1 和玩家 2 基于这个数组设计了一个游戏。<BR/>
+ * 玩家 1 和玩家 2 轮流进行自己的回合，玩家 1 先手。开始时，两个玩家的初始分值都是 0 。<BR/>
+ * 每一回合，玩家从数组的任意一端取一个数字（即，nums[0] 或 nums[nums.length - 1]），取到的数字将会从数组中移除（数组长度减 1 ）。<BR/>
+ * 玩家选中的数字将会加到他的得分上。当数组中没有剩余数字可取时，游戏结束。<BR/>
+ * 如果玩家 1 能成为赢家，返回 true 。如果两个玩家得分相等，同样认为玩家 1 是游戏的赢家，也返回 true 。你可以假设每个玩家的玩法都会使他的分数最大化。<BR/>
+ * 来源：力扣（LeetCode）<BR/>
+ * 链接：<a href="https://leetcode.cn/problems/predict-the-winner">预测赢家</a><BR/>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。<BR/>
+ * &#064;author:  zwy<BR/>
+ * &#064;date:  2022年08月09日 15:50<BR/>
+ */
+public class PredictTheWinner {
+
+}
