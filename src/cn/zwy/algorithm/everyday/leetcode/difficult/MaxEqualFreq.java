14e4fcb8e0392a0a34819173f9c49db68e74fe57
zwy <xiaoyang@heypad.cn>
2022-08-18 16:08:47 +0800

    1224. 最大相等频率 MaxEqualFreq https://leetcode.cn/problems/maximum-equal-frequency/

---
+++ src/cn/zwy/algorithm/everyday/leetcode/difficult/MaxEqualFreq.java
@@ -0,0 +1,10 @@
+package cn.zwy.algorithm.everyday.leetcode.difficult;
+
+/**
+ * &#064;Description:  <a href="https://leetcode.cn/problems/maximum-equal-frequency/">1224. 最大相等频率</a><BR/>
+ * &#064;author:  zwy<BR/>
+ * &#064;date:  2022年08月18日 14:07<BR/>
+ */
+public class MaxEqualFreq {
+
+}
